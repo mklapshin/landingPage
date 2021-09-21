fc35354c007cd025dc4040342eeb9b07049193f4
mklapshin <golub.97@mail.ru>
2021-09-21 13:22:25 +0300

    working with JS courusel

--- index.js
+++ index.js
@@ -1,183 +1,154 @@
  function init() {
-let a = 0;
-    show_add.addEventListener('click', function (event) {
-       a += 1;
-       if (a%2 == 0) {
-        add1.style.display = 'block'; 
-       } else {
-        add1.style.display = 'none'; 
-       }
-    });
 
-// Set the date we're counting down to
-var countDownDate = new Date("Aug 11, 2022 17:57:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get todays date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in an element with id="demo"
-  // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  // + minutes + "m " + seconds + "s ";
-
-  document.getElementById("demo").innerHTML = "0" + hours + ":"
-  + minutes + ":" + seconds;
-  demo.style.fontSize = '80px'; 
-
- 
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
-let counter_car = 0;
-
-    car1.addEventListener('click', function (event) {  
-        counter_car = 0;
-        courusel1_1.style.display = 'block'; 
-        courusel1_2.style.display = 'block';      
-        courusel_c.style.display = 'none';
-        courusel_d.style.display = 'none';
-        courusel3_1.style.display = 'none';
-        courusel2_1.style.display = 'none';
-        car2.style.border = '1px solid #EBEBEB';
-        car1.style.border = '1px solid #FED100';
-        car3.style.border = '1px solid #EBEBEB';
-    });
-
-    car2.addEventListener('click', function (event) {  
-        counter_car = 1;
-        courusel_c.style.display = 'block'; 
-        courusel2_1.style.display = 'block';      
-        courusel1_2.style.display = 'none';
-        courusel_d.style.display = 'none';
-        courusel1_1.style.display = 'none';
-        courusel3_1.style.display = 'none';
-        car2.style.border = '1px solid #FED100';
-        car1.style.border = '1px solid #EBEBEB';
-        car3.style.border = '1px solid #EBEBEB';
-    });
-
-    car3.addEventListener('click', function (event) {  
-        counter_car = 2;
-        courusel_d.style.display = 'block'; 
-        courusel3_1.style.display = 'block';      
-        courusel_c.style.display = 'none';
-        courusel1_2.style.display = 'none';
-        courusel1_1.style.display = 'none';
-        courusel2_1.style.display = 'none';
-        car2.style.border = '1px solid #EBEBEB';
-        car1.style.border = '1px solid #EBEBEB';
-        car3.style.border = '1px solid #FED100';
-    });
-
-    courusel_btn1.addEventListener('click', function (event) {  
+const courusel = () => {
+    let start = Date.now();
+                        
+    let timer = setInterval(function() {
+        let timePassed = Date.now() - start;
+                    
+        train.style.right = timePassed / 0.8 + 'px';
         
-        if (counter_car == 0) {
-            counter_car += 1;
-            courusel1_1.style.display = 'none';      
-            courusel2_1.style.display = 'block'; 
-           // document.getElementById("courusel_change").innerHTML = "<h3>Газель 3 метра</h3><p>Ширина 2 м</p><p>Высота 2 м</p><p>Объём 16 м<sup>3</sup></p><p>Грузоподъёмность 1,5 т</p><button>Заказать</button>"; 
-           // courusel_change.style.display = 'flex'; 
-           // courusel_change.style.flexDirection = 'column';
-            courusel1_2.style.display = 'none';  
-            courusel_c.style.display = 'block';
-            car2.style.border = '1px solid #FED100';
-            car1.style.border = '1px solid #EBEBEB';
-            car3.style.border = '1px solid #EBEBEB';
-        } else if (counter_car == 1) {
-            counter_car += 1;
-            courusel_c.style.display = 'none';
-            courusel_d.style.display = 'block';  
-            courusel2_1.style.display = 'none'; 
-            courusel3_1.style.display = 'block'; 
-            car2.style.border = '1px solid #EBEBEB';
-            car1.style.border = '1px solid #EBEBEB';
-            car3.style.border = '1px solid #FED100';
-        }
-        else if (counter_car == 2) {
-            counter_car = 0;
-            courusel_d.style.display = 'none';
-            courusel1_2.style.display = 'block';  
-            courusel3_1.style.display = 'none'; 
-            courusel1_1.style.display = 'block';   
-            car2.style.border = '1px solid #EBEBEB';
-            car1.style.border = '1px solid #FED100';
-            car3.style.border = '1px solid #EBEBEB';
-        }
-    });
+           
+        if (timePassed > 5000) clearInterval(timer);
+                    
+    }, 20);
+}
+
+const courusel2 = () => {
+    let start = Date.now();
+                        
+    let timer = setInterval(function() {
+        let timePassed = Date.now() - start;
+                    
+        train.style.left = timePassed / 0.8 + 'px';
+                    
+        if (timePassed > 5000) clearInterval(timer);
+                    
+    }, 20);
+}
+
+const courusel4 = () => {
+    let start = Date.now();
+                        
+    let timer = setInterval(function() {
+        let timePassed = Date.now() - start;
+                    
+        train3.style.left = timePassed / 0.8 + 'px';
+                    
+        if (timePassed > 5000) clearInterval(timer);
+                    
+    }, 20);
+}
 
 
-    courusel_btn2.addEventListener('click', function (event) {  
+    const prev = document.getElementById('prev');
+    const next = document.getElementById('next');
+    const slides = document.querySelectorAll('.slider__slides');
+    const btn = document.querySelectorAll('.types__cars');
+    const dots = document.querySelectorAll('.slider__dots')
+    let index = 0;
+    
+    const activeSlide = number =>{
+        for(let slide of slides){
+            slide.classList.remove("active-slide");
+        }
+        slides[number].classList.add("active-slide");
+    }
+    
+    const activeBtn = number =>{
+        for(let button of btn){
+            button.classList.remove("active");
+        }
+        btn[number].classList.add("active");
+    }
+    
+    const activeDot = number =>{
+        for(let dot of dots){
+            dot.classList.remove("active-dot");
+        }
+        dots[number].classList.add("active-dot");
+    }
+    
+    const currentSlide = ind =>{
+        activeSlide(ind);
+        activeBtn(ind);
+        activeDot(ind);
+    }
+    
+    const nextSlide1 = () => {
         
-        if (counter_car == 0) {
-            counter_car += 1;
-            courusel1_1.style.display = 'none';      
-            courusel2_1.style.display = 'block'; 
-           // document.getElementById("courusel_change").innerHTML = "<h3>Газель 3 метра</h3><p>Ширина 2 м</p><p>Высота 2 м</p><p>Объём 16 м<sup>3</sup></p><p>Грузоподъёмность 1,5 т</p><button>Заказать</button>"; 
-           // courusel_change.style.display = 'flex'; 
-           // courusel_change.style.flexDirection = 'column';
-            courusel1_2.style.display = 'none';  
-            courusel_c.style.display = 'block';
-            car2.style.border = '1px solid #FED100';
-            car1.style.border = '1px solid #EBEBEB';
-            car3.style.border = '1px solid #EBEBEB';
-        } else if (counter_car == 1) {
-            counter_car += 1;
-            courusel_c.style.display = 'none';
-            courusel_d.style.display = 'block';  
-            courusel2_1.style.display = 'none'; 
-            courusel3_1.style.display = 'block'; 
-            car2.style.border = '1px solid #EBEBEB';
-            car1.style.border = '1px solid #EBEBEB';
-            car3.style.border = '1px solid #FED100';
+        if(index == slides.length - 1){
+            index = 0;
+            currentSlide(index);
+        }else{
+            index++;
+            currentSlide(index);
         }
-        else if (counter_car == 2) {
-            counter_car = 0;
-            courusel_d.style.display = 'none';
-            courusel1_2.style.display = 'block';  
-            courusel3_1.style.display = 'none'; 
-            courusel1_1.style.display = 'block';   
-            car2.style.border = '1px solid #EBEBEB';
-            car1.style.border = '1px solid #FED100';
-            car3.style.border = '1px solid #EBEBEB';
-        }
-    });
- 
 
-    reviews__btn1.addEventListener('click', function (event) {  
-        reviews01.style.display = 'none';
-        reviews_1.style.display = 'block';
-    });
+        const move = document.querySelector('.active-slide');
+        let start = Date.now();
+                        
+        let timer = setInterval(function() {
+            let timePassed = Date.now() - start;
+                        
+            move.style.right = timePassed / 0.8 + 'px';
+            
+               
+            if (timePassed > 5000) clearInterval(timer);
+                        
+        }, 20);
+    
+    
+    }
+    
+    const prevSlide1 = () => { 
+        if(index == 0){
+            index = slides.length - 1;
+            currentSlide(index);            
+        }else{
+            index--;
+            
+            currentSlide(index);           
+        }     
 
-    reviews__btn2.addEventListener('click', function (event) {  
-        reviews02.style.display = 'none';
-        reviews_2.style.display = 'block';
-    });
+    }
 
-    reviews__btn3.addEventListener('click', function (event) {  
-        reviews03.style.display = 'none';
-        reviews_3.style.display = 'block';
-    });
+    const prevSlide = () => {
+        courusel();
+        setTimeout(prevSlide1, 700)
+        console.log('sdfsdf')
+    }
 
+    const nextSlide = () => {
+        courusel2();
+        setTimeout(nextSlide1, 700)
+    }
+    
+    dots.forEach((item , indexDot) => {
+        item.addEventListener("click", () => {
+            index = indexDot;
+            currentSlide(index);
+        })
+    });
+    
+    btn.forEach((item , indexBtn) => {
+        item.addEventListener("click", () => {
+            index = indexBtn;
+            currentSlide(index);
+        })
+    });
+    
+    next.addEventListener("click", nextSlide);
+    prev.addEventListener("click", prevSlide);
+    
+    
  }
 
 
-
 window.addEventListener('load', init);
+
+
 
 
 
